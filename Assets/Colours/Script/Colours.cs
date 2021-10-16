8751c6b627229cedd1ed67f03acec1559c94d93c
oneRain <onerain88@gmail.com>
2021-10-16 20:26:05 +0800

    init: color values

---
+++ Assets/Colours/Script/Colours.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colours {
+    #region System Color
+
+    public static readonly Color InfoBlueColor = new Color32(47, 112, 225, 255);
+    public static readonly Color SuccessColor = new Color32(83, 215, 106, 255);
+    public static readonly Color WarningColor = new Color32(221, 170, 59, 255);
+    public static readonly Color DangerColor = new Color32(229, 0, 15, 255);
+
+    #endregion
+
+    #region Whites
+
+    public static readonly Color White = new Color32(255, 255, 255, 255);
+    public static readonly Color AntiqueWhiteColor = new Color32(250, 235, 215, 255);
+    public static readonly Color OldLaceColor = new Color32(253, 245, 230, 255);
+    public static readonly Color IvoryColor = new Color32(255, 255, 240, 255);
+    public static readonly Color SeashellColor = new Color32(255, 245, 238, 255);
+    public static readonly Color GhostWhiteColor = new Color32(248, 248, 255, 255);
+    public static readonly Color SnowColor = new Color32(255, 250, 250, 255);
+    public static readonly Color LinenColor = new Color32(250, 240, 230, 255);
+
+    #endregion
+
+    #region Grays
+
+    public static readonly Color Black25PercentColor = new Color32(64, 64, 64, 255);
+    public static readonly Color Black50PercentColor = new Color32(128, 128, 128, 255);
+    public static readonly Color Black75PercentColor = new Color32(192, 192, 192, 255);
+    public static readonly Color Black = new Color32(0, 0, 0, 255);
+    public static readonly Color WarmGrayColor = new Color32(133, 117, 112, 255);
+    public static readonly Color CoolGrayColor = new Color32(118, 122, 133, 255);
+    public static readonly Color CharcoalColor = new Color32(34, 34, 34, 255);
+
+    #endregion
+
+    #region Blues
+
+    public static readonly Color TealColor = new Color32(28, 160, 170, 255);
+    public static readonly Color SteelBlueColor = new Color32(103, 153, 170, 255);
+    public static readonly Color RobinEggColor = new Color32(141, 218, 247, 255);
+    public static readonly Color PastelBlueColor = new Color32(99, 161, 247, 255);
+    public static readonly Color TurquoiseColor = new Color32(112, 219, 219, 255);
+    public static readonly Color SkyBlueColor = new Color32(0, 178, 238, 255);
+    public static readonly Color IndigoColor = new Color32(13, 79, 139, 255);
+    public static readonly Color DenimColor = new Color32(67, 114, 170, 255);
+    public static readonly Color BlueberryColor = new Color32(89, 113, 173, 255);
+    public static readonly Color CornflowerColor = new Color32(100, 149, 237, 255);
+    public static readonly Color BabyBlueColor = new Color32(190, 220, 230, 255);
+    public static readonly Color MidnightBlueColor = new Color32(13, 26, 35, 255);
+    public static readonly Color FadedBlueColor = new Color32(23, 137, 155, 255);
+    public static readonly Color IcebergColor = new Color32(200, 213, 219, 255);
+    public static readonly Color WaveColor = new Color32(102, 169, 251, 255);
+
+    #endregion
+
+    #region Greens
+
+    public static readonly Color EmeraldColor = new Color32(1, 152, 117, 255);
+    public static readonly Color GrassColor = new Color32(99, 214, 74, 255);
+    public static readonly Color PastelGreenColor = new Color32(126, 242, 124, 255);
+    public static readonly Color SeafoamColor = new Color32(77, 226, 140, 255);
+    public static readonly Color PaleGreenColor = new Color32(176, 226, 172, 255);
+    public static readonly Color CactusGreenColor = new Color32(99, 111, 87, 255);
+    public static readonly Color ChartreuseColor = new Color32(69, 139, 0, 255);
+    public static readonly Color HollyGreenColor = new Color32(32, 87, 14, 255);
+    public static readonly Color OliveColor = new Color32(91, 114, 34, 255);
+    public static readonly Color OliveDrabColor = new Color32(107, 142, 35, 255);
+    public static readonly Color MoneyGreenColor = new Color32(134, 198, 124, 255);
+    public static readonly Color HoneydewColor = new Color32(216, 255, 231, 255);
+    public static readonly Color LimeColor = new Color32(56, 237, 56, 255);
+    public static readonly Color CardTableColor = new Color32(87, 121, 107, 255);
+
+    #endregion
+
+    #region Reds
+
+    public static readonly Color SalmonColor = new Color32(233, 87, 95, 255);
+    public static readonly Color BrickRedColor = new Color32(151, 27, 16, 255);
+    public static readonly Color EasterPinkColor = new Color32(241, 167, 162, 255);
+    public static readonly Color GrapefruitColor = new Color32(228, 31, 54, 255);
+    public static readonly Color PinkColor = new Color32(255, 95, 154, 255);
+    public static readonly Color IndianRedColor = new Color32(205, 92, 92, 255);
+    public static readonly Color StrawberryColor = new Color32(190, 38, 37, 255);
+    public static readonly Color CoralColor = new Color32(240, 128, 128, 255);
+    public static readonly Color MaroonColor = new Color32(80, 4, 28, 255);
+    public static readonly Color WatermelonColor = new Color32(242, 71, 63, 255);
+    public static readonly Color TomatoColor = new Color32(255, 99, 71, 255);
+    public static readonly Color PinkLipstickColor = new Color32(255, 105, 180, 255);
+    public static readonly Color PaleRoseColor = new Color32(255, 228, 225, 255);
+    public static readonly Color CrimsonColor = new Color32(187, 18, 36, 255);
+
+    #endregion
+
+    #region Purples
+
+    public static readonly Color EggplantColor = new Color32(105, 5, 98, 255);
+    public static readonly Color PastelPurpleColor = new Color32(207, 100, 235, 255);
+    public static readonly Color PalePurpleColor = new Color32(229, 180, 235, 255);
+    public static readonly Color CoolPurpleColor = new Color32(140, 93, 228, 255);
+    public static readonly Color VioletColor = new Color32(191, 95, 255, 255);
+    public static readonly Color PlumColor = new Color32(139, 102, 139, 255);
+    public static readonly Color LavenderColor = new Color32(204, 153, 204, 255);
+    public static readonly Color RaspberryColor = new Color32(135, 38, 87, 255);
+    public static readonly Color FuschiaColor = new Color32(255, 20, 147, 255);
+    public static readonly Color GrapeColor = new Color32(54, 11, 88, 255);
+    public static readonly Color PeriwinkleColor = new Color32(135, 159, 237, 255);
+    public static readonly Color OrchidColor = new Color32(218, 112, 214, 255);
+
+    #endregion
+
+    #region Yellows
+
+    public static readonly Color GoldenrodColor = new Color32(215, 170, 51, 255);
+    public static readonly Color YellowGreenColor = new Color32(192, 242, 39, 255);
+    public static readonly Color BananaColor = new Color32(229, 227, 58, 255);
+    public static readonly Color MustardColor = new Color32(205, 171, 45, 255);
+    public static readonly Color ButtermilkColor = new Color32(254, 241, 181, 255);
+    public static readonly Color GoldColor = new Color32(139, 117, 18, 255);
+    public static readonly Color CreamColor = new Color32(240, 226, 187, 255);
+    public static readonly Color LightCreamColor = new Color32(240, 238, 215, 255);
+    public static readonly Color WheatColor = new Color32(240, 238, 215, 255);
+    public static readonly Color BeigeColor = new Color32(245, 245, 220, 255);
+
+    #endregion
+
+    #region Oranges
+
+    public static readonly Color PeachColor = new Color32(242, 187, 97, 255);
+    public static readonly Color BurntOrangeColor = new Color32(184, 102, 37, 255);
+    public static readonly Color PastelOrangeColor = new Color32(248, 197, 143, 255);
+    public static readonly Color CantaloupeColor = new Color32(250, 154, 79, 255);
+    public static readonly Color CarrotColor = new Color32(237, 145, 33, 255);
+    public static readonly Color MandarinColor = new Color32(247, 145, 55, 255);
+
+    #endregion
+
+    #region Browns
+
+    public static readonly Color ChiliPowderColor = new Color32(199, 63, 23, 255);
+    public static readonly Color BurntSiennaColor = new Color32(138, 54, 15, 255);
+    public static readonly Color ChocolateColor = new Color32(94, 38, 5, 255);
+    public static readonly Color CoffeeColor = new Color32(141, 60, 15, 255);
+    public static readonly Color CinnamonColor = new Color32(123, 63, 9, 255);
+    public static readonly Color AlmondColor = new Color32(196, 142, 72, 255);
+    public static readonly Color EggshellColor = new Color32(252, 230, 201, 255);
+    public static readonly Color SandColor = new Color32(222, 182, 151, 255);
+    public static readonly Color MudColor = new Color32(70, 45, 29, 255);
+    public static readonly Color SiennaColor = new Color32(160, 82, 45, 255);
+    public static readonly Color DustColor = new Color32(236, 214, 197, 255);
+
+    #endregion
+}
